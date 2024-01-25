6199f08d03570f0472209e800473aa897d80aa06
Yevhen Zelinskyi <3elinskiy@gmail.com>
2024-01-25 09:27:38 +0200

    Initial commit

---
+++ Flash-Chat-App/Flash-Chat-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash-Chat-App
+//
+//  Created by Evgeniy Zelinskiy on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
